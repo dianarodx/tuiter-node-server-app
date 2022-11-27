570c1eafdf4000627c6cee5c9e96ad399cef7e13
dianarodx <diana.rodriguez345@gmail.com>
2022-11-27 12:40:35 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors'
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 app.listen(process.env.PORT || 4000);
 app.use(cors())
